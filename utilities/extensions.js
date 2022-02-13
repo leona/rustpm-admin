2d724c0fc3e84ca6d842e629c5a3ad35d18f2144
leon <leon.harvey@tapadoo.com>
2022-02-13 21:24:14 +0000

    Update getTitle

--- utilities/extensions.js
+++ utilities/extensions.js
@@ -1,7 +1,7 @@
 import { capitalise } from './string.js'
 
 export function getTitle() {
-  let paths = this.$route.path.split('/').filter(x => x.length).map(x => x.toUpperCase())
+  let paths = this.$route.path.split('/').filter(x => x.length).map(x => capitalise(x))
   let title = ''
 
   if (paths.length) {
